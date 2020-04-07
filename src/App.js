5abbd417c7463a155385a9383c9f3b23a5ebd70e
Tacoholic <solischristian@gmail.com>
2020-04-06 21:54:59 -0500

    fixes

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
           Learn React
         </a>
       </header>
-    <DadJokesApi />
+     <DadJokesApi />
     </div>
   );
 }
--- src/DadJokesApi.js
+++ src/DadJokesApi.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+
  class DadJokesApi extends React.Component {
     constructor(props){
         super(props);
