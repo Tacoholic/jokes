25259ce94ba1acc33183441fecbdbc0d33a776f3
Tacoholic <solischristian@gmail.com>
2020-04-14 20:44:37 -0500

    functioning button for SportsJokesApi

--- src/SportsJokesApi.js
+++ src/SportsJokesApi.js
@@ -1,21 +1,33 @@
 import React from 'react'
 import SportsJokesData from './SportsJokesData';
 
+const initialState = {
+  randomJoke: []
+};
+
 class SportsJokesApi extends React.Component {
 
-    getRandomJoke(){
-       return SportsJokesData[(SportsJokesData.length * Math.random()) << 0]
+  constructor(props) {
+    super(props);
+    this.getRandomJoke = this.getRandomJoke.bind(this);
+    this.state = initialState;
+  }
 
-    }
+  getRandomJoke() {
+    this.setState({
+      randomJoke: SportsJokesData[(SportsJokesData.length * Math.random()) << 0]
+    });
+  }
 
-    render() {
-        const randomJoke =  this.getRandomJoke()
-        return (
-            <React.Fragment>
-            <p>{randomJoke.question}</p>
-            <h1>{randomJoke.answer}</h1>
-            </React.Fragment>           
-        )
-        }
-    }
-          export default SportsJokesApi;
+  render() {
+    const { randomJoke } = this.state;
+    return (
+      <React.Fragment >
+      <p> {randomJoke.question} </p> 
+      <p> {randomJoke.answer} </p>
+      <button onClick = {this.getRandomJoke}>click here </button> 
+      </React.Fragment >
+    );
+  }
+}
+export default SportsJokesApi;
