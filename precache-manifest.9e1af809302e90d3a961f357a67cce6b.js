cfb7247317afa5e28d7c9bd06982fba31c3f6c22
Tacoholic <solischristian@gmail.com>
2020-05-02 17:34:08 -0500

    Updates

---
+++ precache-manifest.9e1af809302e90d3a961f357a67cce6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e319d1f978501a29ce2b4bfebde37783",
+    "url": "/jokes/index.html"
+  },
+  {
+    "revision": "d039dab031fee76fe972",
+    "url": "/jokes/static/css/main.ae1f8702.chunk.css"
+  },
+  {
+    "revision": "305f360fb5dfaf4352d7",
+    "url": "/jokes/static/js/2.dabb02d1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/jokes/static/js/2.dabb02d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d039dab031fee76fe972",
+    "url": "/jokes/static/js/main.8f5cd3bd.chunk.js"
+  },
+  {
+    "revision": "9f002f502a0eb2d15ede",
+    "url": "/jokes/static/js/runtime-main.a5588342.js"
+  }
+]);
